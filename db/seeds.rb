ec118aa07885fb43e7055452891267d15bea1b36
scoyne <scoyne@bw.edu>
2018-05-15 15:48:02 -0400

    added seed data to blocipedia

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+# Create Users
+5.times do
+  User.create!(
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
+# Create Wikis
+15.times do
+  wiki = Wiki.create!(
+    user: users.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+wikis = Wiki.all
+
+user = User.first
+user.update_attributes!(
+  email: 'coyne99@gmail.com',
+  password: 'helloworld'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
