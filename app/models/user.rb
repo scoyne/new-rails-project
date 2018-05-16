4a62b16e103f0aa91a1c15a57a1f4f463ab7d9bd
scoyne <scoyne@bw.edu>
2018-05-15 22:54:13 -0400

    new wiki troubleshooting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :wikis
+  has_many :wikis, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :wikis
+  resources :welcome
+
   devise_for :users, controllers: { confirmation: 'confirmations' }
 
-  get 'welcome/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'wikis#index'
 end
